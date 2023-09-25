5f5289db10c17987218c76ed5f998f834dc7e407
kapil choudhary <kapilchoudhary0123.com>
2023-09-25 17:16:34 +0530

    'new commit'

--- src/Components/Navigation Buttons/NavBtns.jsx
+++ src/Components/Navigation Buttons/NavBtns.jsx
@@ -1,59 +1,62 @@
 import "./NavBtn.css";
-import { NavLink } from "react-router-dom";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { useRef } from "react";
+import {NavLink} from "react-router-dom";
+import {FaBars, FaTimes} from "react-icons/fa";
+import {useRef} from "react";
 
 function NavBtns() {
-  const navRef = useRef();
-  const showNavButtons = () => {
-    navRef.current["classList"].toggle("show_nav_btn");
-  };
+    const navRef = useRef();
+    const toggleNavButtons = () => {
+        navRef.current["classList"].toggle("show_nav_btn");
+    };
+    const hideNavButtons = () => {
+        navRef.current["classList"].remove("show_nav_btn");
+    }
 
-  return (
-    <>
-      <ul className={"nav__btn__container"} ref={navRef}>
-        <li className={"nav__btn__list"}>
-          <NavLink className={"nav__link"} to={"home"}>
-            Home
-          </NavLink>
-        </li>
-        <li className={"nav__btn__list"}>
-          <NavLink className={"nav__link"} to={"about"}>
-            About
-          </NavLink>
-        </li>
-        <li className={"nav__btn__list"}>
-          <NavLink className={"nav__link"} to={"categories"}>
-            Categories
-          </NavLink>
-        </li>
-        <li className={"nav__btn__list"}>
-          <NavLink className={"nav__link"} to={"contact"}>
-            Contact
-          </NavLink>
-        </li>
-        <li className={"nav__btn__list"}>
-          <NavLink className={"nav__link"} to={"publish"}>
-            Publish
-          </NavLink>
-        </li>
-        <li>
-          <button
-            className="nav__close__btn nav__toggle__btn"
-            onClick={showNavButtons}
-          >
-            <FaTimes size={'24'}></FaTimes>
-          </button>
-        </li>
-      </ul>
-      <button
-        className="nav__open__btn nav__toggle__btn"
-        onClick={showNavButtons}
-      >
-        <FaBars size={'24'}></FaBars>
-      </button>
-    </>
-  );
+    return (
+        <>
+            <ul className={"nav__btn__container"} ref={navRef}>
+                <li className={"nav__btn__list"}>
+                    <NavLink className={"nav__link"} to={"home"} onClick={hideNavButtons}>
+                        Home
+                    </NavLink>
+                </li>
+                <li className={"nav__btn__list"}>
+                    <NavLink className={"nav__link"} to={"about"} onClick={hideNavButtons}>
+                        About
+                    </NavLink>
+                </li>
+                <li className={"nav__btn__list"}>
+                    <NavLink className={"nav__link"} to={"categories"} onClick={hideNavButtons}>
+                        Categories
+                    </NavLink>
+                </li>
+                <li className={"nav__btn__list"}>
+                    <NavLink className={"nav__link"} to={"contact"} onClick={hideNavButtons}>
+                        Contact
+                    </NavLink>
+                </li>
+                <li className={"nav__btn__list"}>
+                    <NavLink className={"nav__link"} to={"publish"} onClick={hideNavButtons}>
+                        Publish
+                    </NavLink>
+                </li>
+                <li>
+                    <button
+                        className="nav__close__btn nav__toggle__btn"
+                        onClick={toggleNavButtons}
+                    >
+                        <FaTimes size={'24'}></FaTimes>
+                    </button>
+                </li>
+            </ul>
+            <button
+                className="nav__open__btn nav__toggle__btn"
+                onClick={toggleNavButtons}
+            >
+                <FaBars size={'24'}></FaBars>
+            </button>
+        </>
+    );
 }
 
 export default NavBtns;
